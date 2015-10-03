0c572292d57640f9ac68ea9d4311bcde29ff54ef
suprafly <info@suprafly.com>
2015-10-03 10:49:23 -0700

    Basic Models
    Zaps and Tasks. Very elementary, still waiting on Zapier's first email ;)

--- zapalyze/main/models.py
+++ zapalyze/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+def User():
+    return get_user_model()
+
+
+class Zap(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=300)
+
+
+class Task(models.Model):
+    zap = models.ForeignKey(Zap)
+    description = models.CharField(max_length=300)
+    timestamp = models.DateTimeField()
+
+
+
+
+
