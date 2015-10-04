5116251c470e7ed4c77f389b68d5755610c8e9c3
suprafly <info@suprafly.com>
2015-10-04 10:54:06 -0700

    Get Daily Task Summary
    Logs user into gmail and grabs the latest task summary.

--- zapalyze/main/views.py
+++ zapalyze/main/views.py
@@ -5,28 +5,55 @@ import requests
 import json
 import base64
 import email
+import datetime
 
+format_YYMMDD = lambda datetime_obj: datetime_obj.strftime('%Y/%m/%d')
+format_daterange = lambda from_date, until_date: "after:%s before:%s" % (format_YYMMDD(from_date), format_YYMMDD(until_date))
+
+def get_daily_task_summary_list(user, social, daterange_str=None):
+    if daterange_str is None:
+        # No range specified is the default case, so get them all
+        daterange_str = "before: %s" % format_YYMMDD(datetime.date.today())
+    api_str = 'https://www.googleapis.com/gmail/v1/users/me/messages?q="in:inbox from:\"contact@zapier.com\" %s subject:\"Your Daily Task Summary\"' % daterange_str
+    response = requests.get(
+        api_str,
+        params={'access_token': social.extra_data['access_token']}
+    )
+    if response:
+        if 'messages' in response.json():
+            return get_mime_messages(social, response.json()['messages'])
+        else:
+            # No messages returned
+            pass
+    else:
+        # No response
+        pass
+
+def get_mime_messages(social, json_msgs):
+    messages = []
+    for msg in json_msgs:
+        email_response = requests.get(
+            'https://www.googleapis.com/gmail/v1/users/me/messages/%s' % msg['id'],
+            params={'access_token': social.extra_data['access_token'], 'format': 'RAW'}
+        )
+        msg = email_response.json()
+        msg_str = base64.urlsafe_b64decode(email_response.json()['raw'].encode('ASCII'))
+        mime_msg = email.message_from_string(msg_str)
+        messages.append(mime_msg)
+    return messages
+
+# -------- Views --------------------------------------------------------------------------------
 
 def index(request):
     user = request.user
     if user.is_authenticated():
         social = user.social_auth.get(provider='google-oauth2')
-        response = requests.get(
-            'https://www.googleapis.com/gmail/v1/users/me/messages?q="in:sent after:2015/10/01 before:2015/10/03"',
-            params={'access_token': social.extra_data['access_token']}
-        )
-        json_msg = response.json()['messages']
-        for msg in json_msg:
-            m_id = msg['id']
+        today = datetime.date.today()
+        yesterday = today - datetime.timedelta(days=1)
+        task_summary_list = get_daily_task_summary_list(user, social, format_daterange(yesterday, today))
 
-            email_response = requests.get(
-                'https://www.googleapis.com/gmail/v1/users/me/messages/%s' % m_id,
-                params={'access_token': social.extra_data['access_token'], 'format': 'RAW'}
-            )
-            msg = email_response.json()
-            msg_str = base64.urlsafe_b64decode(email_response.json()['raw'].encode('ASCII'))
-            mime_msg = email.message_from_string(msg_str)
-            print mime_msg
+        for item in task_summary_list:
+            print item
 
     ctx = {'request': request, 'user': user}
     context = RequestContext(request, ctx)
