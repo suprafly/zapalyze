a22b3df74ef7743e02e633d30856914a6abdbaee
suprafly <info@suprafly.com>
2015-10-03 17:05:18 -0700

    Gmail Auth and Message Parse
    Logs a user in using the Google Oauth2
    
    Then grabs a selection of their email and parses the messages.

--- zapalyze/main/views.py
+++ zapalyze/main/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
-# Create your views here.
+from django.template.context import RequestContext
+import requests
+import json
+import base64
+import email
+
+
+def index(request):
+    user = request.user
+    if user.is_authenticated():
+        social = user.social_auth.get(provider='google-oauth2')
+        response = requests.get(
+            'https://www.googleapis.com/gmail/v1/users/me/messages?q="in:sent after:2015/10/01 before:2015/10/03"',
+            params={'access_token': social.extra_data['access_token']}
+        )
+        json_msg = response.json()['messages']
+        for msg in json_msg:
+            m_id = msg['id']
+
+            email_response = requests.get(
+                'https://www.googleapis.com/gmail/v1/users/me/messages/%s' % m_id,
+                params={'access_token': social.extra_data['access_token'], 'format': 'RAW'}
+            )
+            msg = email_response.json()
+            msg_str = base64.urlsafe_b64decode(email_response.json()['raw'].encode('ASCII'))
+            mime_msg = email.message_from_string(msg_str)
+            print mime_msg
+
+    ctx = {'request': request, 'user': user}
+    context = RequestContext(request, ctx)
+    return render_to_response('main/index.html', context_instance=context)
+
+
