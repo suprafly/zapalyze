e89ee99d768ff8da073524e95d5fe2abceeaa557
suprafly <info@suprafly.com>
2015-10-09 20:53:15 -0700

    Remove TSV API endpoint
    No longer need it since we have switched to GoogleCharts.

--- zapalyze/API/urls.py
+++ zapalyze/API/urls.py
@@ -6,6 +6,5 @@ from API import views
 
 urlpatterns = [
     # API Get calls
-    url(r'^get_task_data_tsv/$', views.get_task_data_tsv, name='get_task_data_tsv'),
     url(r'^get_task_data_table_json/$', views.get_task_data_table_json, name='get_task_data_table_json'),
 ]
